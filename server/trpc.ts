ac70c96c6afbb8eec3e785b55cd68169314ed46c
Aryan Bhat <aryanbhat2527@gmail.com>
2025-07-26 03:20:21 +0530

    removed inconsistent code

--- server/trpc.ts
+++ server/trpc.ts
@@ -6,9 +6,5 @@ import { initTRPC } from '@trpc/server';
  */
 const t = initTRPC.create();
  
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
 export const router = t.router;
 export const publicProcedure = t.procedure;
