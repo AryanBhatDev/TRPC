360e92bfdfbd79f5f2155b8af94978fab874460b
Aryan Bhat <aryanbhat2527@gmail.com>
2025-07-26 03:21:27 +0530

    removed inconsistent code

--- client/index.ts
+++ client/index.ts
@@ -2,18 +2,18 @@ import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../server';
 
 const trpc = createTRPCClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3001',
-    }),
-  ],
+    links: [
+        httpBatchLink({
+            url: 'http://localhost:3001',
+        }),
+    ],
 });
 
-async function main(){
+async function main() {
     await trpc.createTodo.mutate({
-        title:"hello",
-        description:"yo",
-        done:false
-    })
+        title: 'hello',
+        description: 'yo',
+        done: false,
+    });
 }
-main()
+main();
